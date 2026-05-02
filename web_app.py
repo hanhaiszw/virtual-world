fba3a5defe06c9f3b2f0518cef92a3fbb596e07c
Barton <zhongwei.sun@ximalaya.com>
2026-05-02 14:37:54 +0800

    修改端口号

--- web_app.py
+++ web_app.py
@@ -510,7 +510,7 @@ if __name__ == "__main__":
     uvicorn.run(
         "web_app:app",
         host="127.0.0.1",
-        port=12300,
+        port=12300,    #
         reload=True,
         log_level="warning",
     )
